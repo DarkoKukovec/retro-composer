e1e77dd010c190f89842db36123ada9117c0cd05
Darko Kukovec <darko@infinum.hr>
2019-12-21 14:51:10 +0100

    Updates

---
+++ precache-manifest.29acc6257be66677e336d8a7b8c968b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79193194a28d2c00a2517ce6743a410",
+    "url": "/retro-composer/index.html"
+  },
+  {
+    "revision": "34594df892d867cc6e76",
+    "url": "/retro-composer/static/js/2.f56629bd.chunk.js"
+  },
+  {
+    "revision": "0890ed1e400c6e2fe93bd99f0d43f2a4",
+    "url": "/retro-composer/static/js/2.f56629bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5460b675b9c881db4f5",
+    "url": "/retro-composer/static/js/main.5dcd7ae4.chunk.js"
+  },
+  {
+    "revision": "6f19b5b8a89583ba77d3",
+    "url": "/retro-composer/static/js/runtime-main.285e1053.js"
+  },
+  {
+    "revision": "0a217aa9f126e70ee5538cf6fb85a88a",
+    "url": "/retro-composer/static/media/logo.0a217aa9.svg"
+  }
+]);
