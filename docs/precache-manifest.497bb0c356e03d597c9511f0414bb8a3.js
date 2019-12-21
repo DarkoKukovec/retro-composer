239d5efcbfbe9c32105514bf291622b4c8535e6e
Darko Kukovec <darko@infinum.hr>
2019-12-21 14:45:38 +0100

    Add the app build

---
+++ docs/precache-manifest.497bb0c356e03d597c9511f0414bb8a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e11bc8ef721966e6848a68320fe59c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34594df892d867cc6e76",
+    "url": "/static/js/2.f56629bd.chunk.js"
+  },
+  {
+    "revision": "0890ed1e400c6e2fe93bd99f0d43f2a4",
+    "url": "/static/js/2.f56629bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21ea6ac1d4c75adfda6e",
+    "url": "/static/js/main.b32f40ee.chunk.js"
+  },
+  {
+    "revision": "e2f33bde46e74fea3234",
+    "url": "/static/js/runtime-main.948794b8.js"
+  },
+  {
+    "revision": "0a217aa9f126e70ee5538cf6fb85a88a",
+    "url": "/static/media/logo.0a217aa9.svg"
+  }
+]);
