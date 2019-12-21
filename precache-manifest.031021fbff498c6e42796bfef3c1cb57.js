3af863d8ede1fb7d367d8e99e9f254fadac629c8
Darko Kukovec <darko@infinum.hr>
2019-12-21 16:51:11 +0100

    Updates

---
+++ precache-manifest.031021fbff498c6e42796bfef3c1cb57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac844c4d7d985b083dc02666e5fa474f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b962c351ebd7015447b6",
+    "url": "/static/js/2.b3dbd1a4.chunk.js"
+  },
+  {
+    "revision": "53a0083f9f287484eb2966465285f2e3",
+    "url": "/static/js/2.b3dbd1a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d741504c87e0029a956",
+    "url": "/static/js/main.55392ebf.chunk.js"
+  },
+  {
+    "revision": "e2f33bde46e74fea3234",
+    "url": "/static/js/runtime-main.948794b8.js"
+  },
+  {
+    "revision": "0a217aa9f126e70ee5538cf6fb85a88a",
+    "url": "/static/media/logo.0a217aa9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/retro-composer/precache-manifest.58434e54f8a8b6900862c744a78c815b.js"
+  "/precache-manifest.031021fbff498c6e42796bfef3c1cb57.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/retro-composer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
